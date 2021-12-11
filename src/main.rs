1a5180b6bb9e63d5de7bb11dc4bfd281a96dc091
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2021-12-11 16:44:50 +0100

    move code for day one

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use core::panic;
+mod day_one;
+
 use std::env;
-use std::fs;
+use day_one::day_one;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,57 +17,4 @@ fn main() {
             "Please specify the AOC day in the format of a two integer number. E.g. '01' or '22'."
         )
     }
-}
-
-fn day_one() {
-    let contents: Vec<usize> = fs::read_to_string("resources/01/sample.txt")
-        .unwrap()
-        .lines()
-        .map(|line| line.parse::<usize>().unwrap())
-        .collect();
-
-    println!("Part 1");
-
-    if contents.len() < 2 {
-        panic!("Contents must be longer than 1!")
-    }
-
-    let mut increases = 0;
-    let mut decreases = 0;
-    for (a, b) in contents[0..contents.len() - 1].iter().zip(&contents[1..]) {
-        if a < b {
-            increases += 1;
-        } else if a > b {
-            decreases += 1;
-        }
-    }
-
-    println!("\tIncreases: {}, Decreases {}", increases, decreases);
-
-    println!("Part 2");
-
-    if contents.len() < 3 {
-        panic!("Contents must be longer than 2!")
-    }
-
-    let mut increases = 0;
-    let mut decreases = 0;
-    let iters: Vec<_> = (0..4)
-        .map(|offset| &contents[offset..contents.len() - 3 + offset])
-        .collect();
-    let iter_1 = iters[0].iter().zip(iters[1]).zip(iters[2]);
-    let iter_2 = iters[1].iter().zip(iters[2]).zip(iters[3]);
-
-    for (a, b) in iter_1.zip(iter_2) {
-        let ((a_1, a_2), a_3) = a;
-        let ((b_1, b_2), b_3) = b;
-
-        if (a_1 + a_2 + a_3) < (b_1 + b_2 + b_3) {
-            increases += 1;
-        } else if (a_1 + a_2 + a_3) > (b_1 + b_2 + b_3) {
-            decreases += 1;
-        }
-    }
-
-    println!("\tIncreases: {}, Decreases {}", increases, decreases);
 }
