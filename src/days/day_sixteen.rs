9d26759f92538ca4ae649095447dbd74b589e458
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2021-12-17 13:37:46 +0100

    part 1 parsing

--- src/days/day_sixteen.rs
+++ src/days/day_sixteen.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::io::{self, BufRead};
 
 pub fn day_sixteen() {
-    let file = fs::File::open("resources/16/example1.txt").unwrap();
+    let file = fs::File::open("resources/16/example4.txt").unwrap();
     let mut lines = io::BufReader::new(file).lines();
     let hexcode = lines.next().unwrap().unwrap();
     let binary = hexcode
@@ -20,10 +20,11 @@ pub fn day_sixteen() {
     println!("Part 2");
 }
 
-fn parse_message(current_message: &str) -> Option<u32> {
+fn parse_message(current_message: &str) -> Option<&str> {
     let version = u32::from_str_radix(current_message.get(0..3)?, 2).ok()?;
     let typeid = u32::from_str_radix(current_message.get(3..6)?, 2).ok()?;
     let mut submessage = current_message.get(6..)?;
+    println!("Version: {}, Type ID: {}", version, typeid);
 
     match typeid {
         4 => {
@@ -35,16 +36,34 @@ fn parse_message(current_message: &str) -> Option<u32> {
             binary_value += submessage.get(1..5)?;
             let value = u32::from_str_radix(binary_value.as_str(), 2).ok()?;
             println!("Value: {}", value);
+            return submessage.get(5..);
         }
         _ => {
             let i = submessage.get(0..1)?;
+            submessage = submessage.get(1..)?;
             match i {
-                "0" => {}
-                "1" => {}
+                "0" => {
+                    let subpacket_length = usize::from_str_radix(submessage.get(..15)?, 2).ok()?;
+                    submessage = submessage.get(15..)?;
+                    let mut subpacket = submessage.get(..subpacket_length)?;
+                    println!("Length type ID: 0, Subpacket Length {}", subpacket_length);
+                    while let Some(x) = parse_message(subpacket) {
+                        subpacket = x;
+                    }
+                    return submessage.get(subpacket_length..);
+                }
+                "1" => {
+                    let num_subpackets = usize::from_str_radix(submessage.get(..11)?, 2).ok()?;
+                    println!("Length type ID: 1, Number of Subpackets {}", num_subpackets);
+                    submessage = submessage.get(11..)?;
+                    for i in 0..num_subpackets {
+                        submessage = parse_message(submessage)?;
+                    }
+                }
                 _ => {}
             }
         }
     }
 
-    Some(version)
+    None
 }
