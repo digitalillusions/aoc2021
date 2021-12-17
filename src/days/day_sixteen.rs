787a6c71c0bc10ecb451e382b66045fcd989d92f
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2021-12-17 13:50:46 +0100

    all examples work, but not the sample

--- src/days/day_sixteen.rs
+++ src/days/day_sixteen.rs
@@ -2,7 +2,7 @@ use std::fs;
 use std::io::{self, BufRead};
 
 pub fn day_sixteen() {
-    let file = fs::File::open("resources/16/example4.txt").unwrap();
+    let file = fs::File::open("resources/16/example7.txt").unwrap();
     let mut lines = io::BufReader::new(file).lines();
     let hexcode = lines.next().unwrap().unwrap();
     let binary = hexcode
@@ -14,17 +14,19 @@ pub fn day_sixteen() {
     println!("Binary {}", binary);
 
     // iterate only while there is still at least a version (3 bits) and packet id (3 bits) and at least one more bit remaining
-    parse_message(&binary);
 
     println!("Part 1");
+    let x = parse_message(&binary).unwrap();
+    println!("\tSum of version numbers {}", x.1);
+
     println!("Part 2");
 }
 
-fn parse_message(current_message: &str) -> Option<&str> {
-    let version = u32::from_str_radix(current_message.get(0..3)?, 2).ok()?;
+fn parse_message(current_message: &str) -> Option<(&str, u32)> {
+    let mut version = u32::from_str_radix(current_message.get(0..3)?, 2).ok()?;
     let typeid = u32::from_str_radix(current_message.get(3..6)?, 2).ok()?;
     let mut submessage = current_message.get(6..)?;
-    println!("Version: {}, Type ID: {}", version, typeid);
+    println!("\tVersion: {}, Type ID: {}", version, typeid);
 
     match typeid {
         4 => {
@@ -35,8 +37,8 @@ fn parse_message(current_message: &str) -> Option<&str> {
             }
             binary_value += submessage.get(1..5)?;
             let value = u32::from_str_radix(binary_value.as_str(), 2).ok()?;
-            println!("Value: {}", value);
-            return submessage.get(5..);
+            println!("\tValue: {}", value);
+            return Some((submessage.get(5..)?, version));
         }
         _ => {
             let i = submessage.get(0..1)?;
@@ -46,24 +48,31 @@ fn parse_message(current_message: &str) -> Option<&str> {
                     let subpacket_length = usize::from_str_radix(submessage.get(..15)?, 2).ok()?;
                     submessage = submessage.get(15..)?;
                     let mut subpacket = submessage.get(..subpacket_length)?;
-                    println!("Length type ID: 0, Subpacket Length {}", subpacket_length);
-                    while let Some(x) = parse_message(subpacket) {
+                    println!("\tLength type ID: 0, Subpacket Length {}", subpacket_length);
+                    while let Some((x, version_sum)) = parse_message(subpacket) {
+                        version += version_sum;
                         subpacket = x;
                     }
-                    return submessage.get(subpacket_length..);
+                    return Some((submessage.get(subpacket_length..)?, version));
                 }
                 "1" => {
                     let num_subpackets = usize::from_str_radix(submessage.get(..11)?, 2).ok()?;
-                    println!("Length type ID: 1, Number of Subpackets {}", num_subpackets);
+                    println!(
+                        "\tLength type ID: 1, Number of Subpackets {}",
+                        num_subpackets
+                    );
                     submessage = submessage.get(11..)?;
                     for i in 0..num_subpackets {
-                        submessage = parse_message(submessage)?;
+                        let x = parse_message(submessage)?;
+                        submessage = x.0;
+                        version += x.1;
                     }
+                    return Some((submessage, version));
                 }
                 _ => {}
             }
         }
     }
 
-    None
+    Some((submessage, version))
 }
