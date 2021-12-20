887d2a2695aefd106e70f39132d6c25aed2e02a5
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2021-12-20 16:13:45 +0100

    no solution yet, have to actually assemble and count the total grid of points

--- src/days/day_nineteen/mod.rs
+++ src/days/day_nineteen/mod.rs
@@ -5,7 +5,7 @@ use std::io::{self, BufRead};
 use vec3::Vec3;
 
 pub fn day_nineteen() {
-    let file = fs::File::open("resources/19/example1.txt").unwrap();
+    let file = fs::File::open("resources/19/sample.txt").unwrap();
     let lines = io::BufReader::new(file).lines();
     let mut scanners = Vec::new();
 
@@ -33,41 +33,60 @@ pub fn day_nineteen() {
         .map(|(i, _)| if i == 0 { Some((0, 0, 0)) } else { None })
         .collect();
 
+    let mut count_matching_beacons = 0;
+    let mut total_readings = 0;
     for (i, scanner) in scanners.iter().enumerate() {
+        total_readings += scanner.len();
         for (other_scanner, j) in scanners
             .get(i + 1..scanners.len())
             .unwrap()
             .iter()
             .zip(i + 1..scanners.len())
         {
-            let rotations_j = rotations(other_scanner);
-            println!("Comparing scanner {} and scanner {}", i, j);
-            for other_rotated_scanner in rotations_j {
-                for point_i in scanner {
-                    for point_j in other_rotated_scanner.iter() {
-                        let offset_ij = point_j - point_i;
-                        let count_matching_points = other_rotated_scanner
+            if let Some(num_match) = check_match_ij(scanner, other_scanner, i, j) {
+                count_matching_beacons += num_match;
+            }
+        }
+    }
+
+    println!("Total number of readings {}\nNumber of beacons counted twice {}\nTotal number of beacons {}", total_readings, count_matching_beacons, total_readings-count_matching_beacons);
+}
+
+fn check_match_ij(
+    scanner: &Vec<Vec3>,
+    other_scanner: &Vec<Vec3>,
+    i: usize,
+    j: usize,
+) -> Option<usize> {
+    let rotations_j = rotations(other_scanner);
+    println!("Comparing scanner {} and scanner {}", i, j);
+    for other_rotated_scanner in rotations_j {
+        for point_i in scanner {
+            for point_j in other_rotated_scanner.iter() {
+                let offset_ij = point_j - point_i;
+                let count_matching_beacons = other_rotated_scanner
+                    .iter()
+                    .filter(|x_j| {
+                        scanner
                             .iter()
-                            .filter(|x_j| {
-                                scanner
-                                    .iter()
-                                    .map(|&x_i| (x_i + offset_ij.clone() - *x_j.clone()).abs())
-                                    .min()
-                                    .unwrap()
-                                    == Vec3::zero()
-                            })
-                            .count();
-                        if count_matching_points >= 12 {
-                            println!(
-                                "Found match between scanner {} and scanner {} with offset {:?}",
-                                i, j, offset_ij
-                            );
-                        }
-                    }
+                            .map(|&x_i| (x_i + offset_ij.clone() - *x_j.clone()).abs())
+                            .min()
+                            .unwrap()
+                            == Vec3::zero()
+                    })
+                    .count();
+                if count_matching_beacons >= 12 {
+                    println!(
+                        "Found match between scanner {} and scanner {} with offset {:?}",
+                        i, j, offset_ij
+                    );
+                    return Some(count_matching_beacons);
                 }
             }
         }
     }
+
+    return None;
 }
 
 fn rotations(points: &Vec<Vec3>) -> Vec<Vec<Vec3>> {
