c6a65bdcd42f1480b29cf3725175900867909a55
Stefan <stefan.jeske@rwth-aachen.de>
2021-12-21 23:26:36 +0100

    Part 1

--- src/days/day_nineteen/mod.rs
+++ src/days/day_nineteen/mod.rs
@@ -1,6 +1,6 @@
 mod vec3;
 
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::io::{self, BufRead};
 use vec3::Vec3;
@@ -13,7 +13,7 @@ pub fn day_nineteen() {
     for line in lines {
         let line_string = line.unwrap();
         if line_string.contains("scanner") {
-            scanners.push(Vec::new());
+            scanners.push(HashSet::new());
         } else if line_string.is_empty() {
             continue;
         } else {
@@ -24,43 +24,76 @@ pub fn day_nineteen() {
             scanners
                 .last_mut()
                 .unwrap()
-                .push(Vec3::from([xyz[0], xyz[1], xyz[2]]));
+                .insert(Vec3::from([xyz[0], xyz[1], xyz[2]]));
         }
     }
 
     let _scanner_coords: Vec<_> = scanners
         .iter()
         .enumerate()
-        .map(|(i, _)| if i == 0 { Some((0, 0, 0)) } else { None })
+        .map(|(i, _)| if i == 0 { Some(Vec3::zero()) } else { None })
         .collect();
 
-    let mut count_matching_beacons = 0;
-    let mut total_readings = 0;
-    for (i, scanner) in scanners.iter().enumerate() {
-        total_readings += scanner.len();
-        for (other_scanner, j) in scanners
-            .get(i + 1..scanners.len())
-            .unwrap()
-            .iter()
-            .zip(i + 1..scanners.len())
-        {
-            if let Some(num_match) = check_match_ij(scanner, other_scanner, i, j) {
-                count_matching_beacons += num_match;
+    // let mut count_matching_beacons = 0;
+    // let mut total_readings = 0;
+    // for (i, scanner) in scanners.iter().enumerate() {
+    //     total_readings += scanner.len();
+    //     for (other_scanner, j) in scanners
+    //         .get(i + 1..scanners.len())
+    //         .unwrap()
+    //         .iter()
+    //         .zip(i + 1..scanners.len())
+    //     {
+    //         if let Some(num_match) = check_match_ij(scanner, other_scanner, i, j) {
+    //             count_matching_beacons += num_match;
+    //         }
+    //     }
+    // }
+
+    let scanner_indices: HashSet<_> = (0..scanners.len()).collect();
+    let mut total_beacons = HashSet::new();
+    let mut already_known = HashSet::new();
+    let mut check_next = HashSet::new();
+    let mut scanner_coords = HashMap::new();
+
+    total_beacons.extend(scanners[0].clone());
+    already_known.insert(0);
+    check_next.insert(0);
+    scanner_coords.insert(0 as usize, Vec3::zero());
+
+    while !check_next.is_empty() {
+        let mut newly_found = HashSet::new();
+        for i in check_next {
+            for &j in scanner_indices.difference(&already_known) {
+                println!("Comparing scanner {} and scanner {}", i, j);
+                if let Some((beacons, offset_ij)) = check_match_ij(&scanners[i], &scanners[j]) {
+                    println!(
+                        "Found match between scanner {} and scanner {} with offset {:?}",
+                        i, j, offset_ij
+                    );
+                    scanner_coords.insert(j, scanner_coords.get(&i).unwrap() - &offset_ij);
+                    newly_found.insert(j);
+                    total_beacons.extend(beacons.clone());
+                    scanners[j] = beacons;
+                }
             }
         }
+        already_known.extend(newly_found.clone());
+        check_next = newly_found;
     }
 
-    println!("Total number of readings {}\nNumber of beacons counted twice {}\nTotal number of beacons {}", total_readings, count_matching_beacons, total_readings-count_matching_beacons);
+    println!("Already known {:?}", already_known);
+    println!("Scanner Coords {:?}", scanner_coords);
+    println!("Total number of beacons {:?}", total_beacons.len());
+
+    // println!("Total number of readings {}\nNumber of beacons counted twice {}\nTotal number of beacons {}", total_readings, count_matching_beacons, total_readings-count_matching_beacons);
 }
 
 fn check_match_ij(
-    scanner: &Vec<Vec3>,
-    other_scanner: &Vec<Vec3>,
-    i: usize,
-    j: usize,
-) -> Option<usize> {
+    scanner: &HashSet<Vec3>,
+    other_scanner: &HashSet<Vec3>,
+) -> Option<(HashSet<Vec3>, Vec3)> {
     let rotations_j = rotations(other_scanner);
-    println!("Comparing scanner {} and scanner {}", i, j);
     for other_rotated_scanner in rotations_j {
         for point_i in scanner {
             for point_j in other_rotated_scanner.iter() {
@@ -77,11 +110,11 @@ fn check_match_ij(
                     })
                     .count();
                 if count_matching_beacons >= 12 {
-                    println!(
-                        "Found match between scanner {} and scanner {} with offset {:?}",
-                        i, j, offset_ij
-                    );
-                    return Some(count_matching_beacons);
+                    let transformed_beacons: HashSet<_> = other_rotated_scanner
+                        .iter()
+                        .map(|&x| x - offset_ij.clone())
+                        .collect();
+                    return Some((transformed_beacons, offset_ij));
                 }
             }
         }
@@ -90,7 +123,7 @@ fn check_match_ij(
     return None;
 }
 
-fn rotations(points: &Vec<Vec3>) -> Vec<Vec<Vec3>> {
+fn rotations(points: &HashSet<Vec3>) -> Vec<HashSet<Vec3>> {
     let mut rotated_points = Vec::new();
     // println!("Original {:?}", points[0]);
     // All possible combinations of 2 axes
@@ -111,7 +144,7 @@ fn rotations(points: &Vec<Vec3>) -> Vec<Vec<Vec3>> {
                         points
                             .iter()
                             .map(|x| x.rotate(rot_axis.clone(), second_axis.clone()))
-                            .collect::<Vec<_>>(),
+                            .collect::<HashSet<_>>(),
                     );
                     // println!(
                     //     "Axis 1 {:?}\nAxis 2 {:?}\nAxis 3 {:?}",
