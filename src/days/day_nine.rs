cdb0e78371ac2a8f13581ccb019d79d29f1c5d91
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2022-01-09 16:12:18 +0100

    finish day 9

--- src/days/day_nine.rs
+++ src/days/day_nine.rs
@@ -44,9 +44,58 @@ pub fn day_nine() {
         }
     }
 
-    println!("\tLow Points: {:?}", low_points);
+    // println!("\tLow Points: {:?}", low_points);
     println!(
         "\tAnswer: {:?}",
         low_points.iter().map(|(x, _, _)| *x + 1).sum::<u32>()
+    );
+
+    println!("Part 2");
+    let mut basins = Vec::new();
+
+    for sink in low_points {
+        let mut basin_to_check = VecDeque::new();
+        let mut basin = HashSet::new();
+
+        basin_to_check.push_back(sink);
+        while let Some((_, i, j)) = basin_to_check.pop_front() {
+            let line = contents.get(i).unwrap();
+            let mut neighbors = Vec::new();
+            if let Some(prev_line_idx) = i.checked_sub(1) {
+                neighbors.push((
+                    contents.get(prev_line_idx).unwrap().get(j).unwrap(),
+                    i - 1,
+                    j,
+                ));
+            }
+            if let Some(next_line) = contents.get(i + 1) {
+                neighbors.push((next_line.get(j).unwrap(), i + 1, j));
+            }
+            if let Some(prev_item_idx) = j.checked_sub(1) {
+                neighbors.push((line.get(prev_item_idx).unwrap(), i, j - 1));
+            }
+            if let Some(next_item) = line.get(j + 1) {
+                neighbors.push((next_item, i, j + 1));
+            }
+            for y in neighbors {
+                if (y.0 < &9) && !basin.contains(&y) {
+                    basin.insert(y);
+                    basin_to_check.push_back(y);
+                }
+            }
+        }
+
+        basins.push((sink, basin.len()));
+    }
+
+    // Reverse sorting
+    basins.sort_by(|x, y| y.1.cmp(&x.1));
+
+    // First 3 elements should be largest
+    let answer = basins[0..3].iter().map(|x| x.1).product::<usize>();
+    println!(
+        "\tLargest basins: {:?}\n\tAnswer: {}",
+        &basins[0..3],
+        answer
     );
 }
