7d2c65d9fdcea5b274280462195f48fd7065a4b9
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2022-01-09 13:43:27 +0100

    Prep part 2

--- src/days/day_nine.rs
+++ src/days/day_nine.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashSet, VecDeque};
 use std::fs;
 use std::io::BufRead;
 
@@ -22,19 +23,23 @@ pub fn day_nine() {
         for (j, x) in line.iter().enumerate() {
             let mut neighbors = Vec::new();
             if let Some(prev_line_idx) = i.checked_sub(1) {
-                neighbors.push(contents.get(prev_line_idx).unwrap().get(j).unwrap());
+                neighbors.push((
+                    contents.get(prev_line_idx).unwrap().get(j).unwrap(),
+                    i - 1,
+                    j,
+                ));
             }
             if let Some(next_line) = contents.get(i + 1) {
-                neighbors.push(next_line.get(j).unwrap());
+                neighbors.push((next_line.get(j).unwrap(), i + 1, j));
             }
             if let Some(prev_item_idx) = j.checked_sub(1) {
-                neighbors.push(line.get(prev_item_idx).unwrap());
+                neighbors.push((line.get(prev_item_idx).unwrap(), i, j - 1));
             }
             if let Some(next_item) = line.get(j + 1) {
-                neighbors.push(next_item);
+                neighbors.push((next_item, i, j + 1));
             }
-            if x < neighbors.iter().min().unwrap() {
-                low_points.push(x);
+            if x < neighbors.iter().min_by(|x, y| x.0.cmp(y.0)).unwrap().0 {
+                low_points.push((x, i, j));
             }
         }
     }
@@ -42,6 +47,6 @@ pub fn day_nine() {
     println!("\tLow Points: {:?}", low_points);
     println!(
         "\tAnswer: {:?}",
-        low_points.iter().map(|x| *x + 1).sum::<u32>()
+        low_points.iter().map(|(x, _, _)| *x + 1).sum::<u32>()
     );
 }
