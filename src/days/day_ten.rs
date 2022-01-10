31bc9f676be046ea82ec44914aeb0a368db4fbac
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2022-01-10 11:52:25 +0100

    day 10 finished

--- src/days/day_ten.rs
+++ src/days/day_ten.rs
@@ -1,27 +1,32 @@
-use std::collections::{VecDeque, HashSet, HashMap};
+use std::collections::{VecDeque, HashMap};
 
 pub fn day_ten(){
-	let contents = std::fs::read_to_string("resources/10/example1.txt").unwrap();
+	let contents = std::fs::read_to_string("resources/10/sample.txt").unwrap();
 
 	let syntax_lines: Vec<Vec<_>> = contents.split("\n").map(|x|{
 		x.trim().chars().collect::<Vec<_>>()
 	}).collect();
 
-	println!("{:?}", syntax_lines);
-
 	let braces = HashMap::from([('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]);
+	let penalty = HashMap::from([(')', 3 as u64), (']', 57), ('}', 1197), ('>', 25137)]);
+	let score = HashMap::from([(')', 1 as u64), (']', 2), ('}', 3), ('>', 4)]);
 
-	println!("{:?}", braces);
-
+	let mut total_penalty = 0;
+	let mut completion_scores = Vec::new();
 	for line in syntax_lines{
 		let mut opened_braces = VecDeque::new();
+		let mut line_corrupt = false;
 		for y in line{
 			if braces.contains_key(&y){
 				opened_braces.push_back(braces.get(&y).unwrap());
 			} else {
 				if let Some(should_close) = opened_braces.pop_back(){
 					if should_close != &y{
-						println!("Syntax Error: Expected {}, found {}", should_close, y);
+						// println!("Syntax Error: Expected {}, found {}", should_close, y);
+						if let Some(val) = penalty.get(&y){
+							total_penalty += val;
+							line_corrupt = true;
+						}
 					}
 				} else {
 					println!("Syntax Error: Closed brace without opening brace.")
@@ -29,10 +34,22 @@ pub fn day_ten(){
 			}
 		}
 
-		if !opened_braces.is_empty(){
-			println!("Corrupted Line found, following braces remain {:?}", opened_braces);
+		if !opened_braces.is_empty() && !line_corrupt{
+			let mut total_score = 0;
+			for y in opened_braces.iter().rev(){
+				if let Some(val) = score.get(y){
+					total_score = total_score*5 + val;
+				}
+			}
+			completion_scores.push(total_score);
+			// println!("Completion Score: {}", total_score);
 		} else {
-			println!("Line OK.");
+			// println!("Line OK.");
 		}
 	}
+
+	completion_scores.sort();
+
+	println!("Total score: {}", total_penalty);
+	println!("Middle completion score: {}", completion_scores.get(completion_scores.len()/2).unwrap());
 }
