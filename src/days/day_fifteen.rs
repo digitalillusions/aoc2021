442ec4753e91c1beafb4d580cd767bdf24c00648
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2022-01-12 16:35:34 +0100

    small optimization

--- src/days/day_fifteen.rs
+++ src/days/day_fifteen.rs
@@ -60,18 +60,24 @@ pub fn day_fifteen() {
 }
 
 fn find_shortest_path(grid: &Vec<Vec<u32>>) {
-    let mut minimum_distance: HashMap<_, _> = grid
-        .iter()
-        .enumerate()
-        .flat_map(|(i, line)| {
-            line.iter()
-                .enumerate()
-                .map(move |(j, _)| ((i, j), u32::MAX))
-        })
-        .collect();
+    // let mut minimum_distance: HashMap<_, _> = grid
+    //     .iter()
+    //     .enumerate()
+    //     .flat_map(|(i, line)| {
+    //         line.iter()
+    //             .enumerate()
+    //             .map(move |(j, _)| ((i, j), u32::MAX))
+    //     })
+    //     .collect();
+    // *minimum_distance.get_mut(&next_node.unwrap()).unwrap() = 0;
+
+    let mut minimum_distance = HashMap::from([((0, 0), 0)]);
     let mut next_node = Some((0 as usize, 0 as usize));
     let mut already_visited = HashSet::new();
-    *minimum_distance.get_mut(&next_node.unwrap()).unwrap() = 0;
+
+    let mut active_set = HashMap::from([((0, 0), 0)]);
+    let grid_len = grid.len() * grid.last().unwrap().len();
+
     while let Some(node) = next_node {
         already_visited.insert(node);
         let cur_value = minimum_distance.get(&node).unwrap().clone();
@@ -82,12 +88,15 @@ fn find_shortest_path(grid: &Vec<Vec<u32>>) {
         neighbors.push(node.1.checked_add(1).map(|y| (node.0, y)));
 
         neighbors.iter().flatten().for_each(|n_node| {
-            if let Some(value) = minimum_distance.get_mut(&n_node) {
-                let new_value =
-                    grid.get(n_node.0).unwrap().get(n_node.1).unwrap() + cur_value.clone();
-                if *value > new_value {
-                    *value = new_value;
-                    // already_visited.remove(n_node);
+            if let Some(neighbor_value) = grid.get(n_node.0).and_then(|line| line.get(n_node.1)) {
+                let new_value = neighbor_value + cur_value.clone();
+                if let Some(value) = minimum_distance.get_mut(n_node) {
+                    if new_value < *value {
+                        *value = new_value;
+                        already_visited.remove(n_node);
+                    }
+                } else {
+                    minimum_distance.insert(*n_node, new_value);
                 }
             }
         });
@@ -95,11 +104,11 @@ fn find_shortest_path(grid: &Vec<Vec<u32>>) {
         next_node = minimum_distance
             .iter()
             .filter(|(x, _)| !already_visited.contains(&x))
-            .min()
+            .min_by(|x, y| x.1.cmp(y.1))
             .map(|x| *x.0);
         print!(
             "\r\tProgress {:.2}%",
-            already_visited.len() as f32 / minimum_distance.keys().len() as f32 * 100.
+            already_visited.len() as f32 / grid_len as f32 * 100.
         )
     }
     println!("");
