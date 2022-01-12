1e252dd37ac10ac2dbea79ab8bd6e87047368ebf
Stefan Jeske <stefan.jeske@rwth-aachen.de>
2022-01-13 00:03:34 +0100

    starting parsing

--- src/days/day_seventeen.rs
+++ src/days/day_seventeen.rs
@@ -1,3 +1,13 @@
 pub fn day_seventeen() {
     let input = std::fs::read_to_string("resources/17/example1.txt").unwrap();
+    let bounds = ["x=", "..", ", ", "y=", ".."];
+    let mut input_str = input.as_str();
+    for bound in bounds.windows(2) {
+        let (start, end) = (
+            input_str.find(bound[0]).unwrap(),
+            input_str.find(bound[1]).unwrap(),
+        );
+        println!("{}", &input_str[start + bound[0].len()..end]);
+        input_str = &input_str[end..];
+    }
 }
